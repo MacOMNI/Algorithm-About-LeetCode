a95de7583c78537816941808ee923d8fe11aca1b
MacKun <4142944942@qq.com>
2020-04-03 12:53:52 +0800

    # try two sum func

--- LeetCode_go/LeetCode/TwoSum.go
+++ LeetCode_go/LeetCode/TwoSum.go
@@ -1,8 +1,12 @@
 package LeetCode
 
+import "fmt"
+
 // two sum
 func twoSum(nums []int, target int) []int {
 	var sum = []int{1, 2}
-
+	for i := 0; i < count; i++ {
+		fmt.Println(nums[i])
+	}
 	return sum
 }
