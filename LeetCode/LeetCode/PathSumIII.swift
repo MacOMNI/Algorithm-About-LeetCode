3228a0070162200517bcc0887f060d0202228c03
MacOMNI <414294494@qq.com>
2020-11-05 15:59:45 +0800

    # update pathsumIII

--- LeetCode/LeetCode/PathSumIII.swift
+++ LeetCode/LeetCode/PathSumIII.swift
@@ -9,14 +9,35 @@
 import Foundation
 
 class PathSum {
+//    func pathSum(_ root: TreeNode?, _ sum: Int) -> Int {
+//        guard let root = root else {
+//            return 0
+//        }
+//        let cnt = (root.val == sum ? 1 : 0) // 满足当前节点
+//        return cnt  + pathSum(root.left,sum) + pathSum(root.right, sum) + pathNodeSum(root.left, sum - root.val) + pathNodeSum(root.right, sum - root.val)
+//    }
+//    func pathNodeSum(_ root: TreeNode?, _ sum: Int) -> Int {
+//        guard let root = root else {
+//            return 0
+//        }
+//        let cnt = (root.val == sum ? 1 : 0) // 满足当前节点
+//
+//       // print(root.val,sum,cnt)
+//        return cnt  + pathNodeSum(root.left, sum - root.val) + pathNodeSum(root.right, sum - root.val)
+//    }
     func pathSum(_ root: TreeNode?, _ sum: Int) -> Int {
-        var cnt = 0
-        if sum == 0 {
-           cnt = 1
-        }
         guard let root = root else {
             return 0
         }
-        return cnt + pathSum(root.left,sum) + pathSum(root.right,sum) + pathSum(root.left,sum - root.val) + pathSum(root.right,sum - root.val)
+        let cnt = (root.val == sum ? 1 : 0) // 满足当前节点
+        return cnt  + pathNodeSum(root.left,sum) + pathNodeSum(root.right, sum) + pathSum(root.left, sum - root.val) + pathSum(root.right, sum - root.val)
+    }
+    func pathNodeSum(_ root: TreeNode?, _ sum: Int) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        let cnt = (root.val == sum ? 1 : 0) // 满足当前节点
+        // print(root.val,sum,cnt)
+        return cnt  + pathNodeSum(root.left, sum - root.val) + pathNodeSum(root.right, sum - root.val)
     }
 }
