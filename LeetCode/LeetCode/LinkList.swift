4196974f6d00a1975a426cd0cc642f30b7d69538
MacOMNI <414294494@qq.com>
2020-12-14 19:35:59 +0800

    # LinkList

--- LeetCode/LeetCode/LinkList.swift
+++ LeetCode/LeetCode/LinkList.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class LinkList: NSObject {
 
+    func isPalindrome(_ head: ListNode?) -> Bool {
+        var head = head
+        if head == nil {
+            return false
+        }
+        var i = 0
+        var fast = head
+        var slow = head
+        while fast?.next != nil && fast?.next?.next != nil {//走一半
+            fast = fast?.next!.next
+            slow = slow?.next!
+            i += 1
+        }
+        // 反转链表
+        var reverHead = ListNode.init(val: -1, next: slow)
+        while slow?.next != nil {
+            
+        }
+        
+        while reverHead.next != nil  {
+            if reverHead.next!.val != head?.val {
+                return false
+            }
+            reverHead = reverHead.next!
+            head = head?.next
+        }
+        
+        return true
+    }
+    
     func rotateRight(_ head: ListNode?, _ k: Int) -> ListNode? {
             guard let head = head else {
                 return nil
